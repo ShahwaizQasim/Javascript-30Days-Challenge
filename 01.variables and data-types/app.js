0583b145aa7d835f9dfe9c81ff940007591a3839
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-07-17 07:20:29 -0700

    2 challenges complete

---
+++ 01.variables and data-types/app.js
@@ -0,0 +1,47 @@
+ var num = 25;
+// console.log(num);
+
+let str = 'Shahwaiz';
+// console.log(str);
+ 
+const boolean = true;
+// console.log(boolean);
+
+// number 
+const numb = 123;
+console.log(typeof numb);
+
+//string
+const stri = "shahwaizqasim";
+console.log(typeof stri);
+
+// boolean 
+const bool = true;
+console.log(typeof bool);
+
+// null 
+const empty = null;
+console.log(typeof empty); // null ki data-type object hai
+
+// Array 
+const myArr = ['ali','qasim','wasid'];
+console.log(typeof myArr); // array ki data type object hai 
+
+// Object 
+const myObj = {
+    name: "Shahwaiz",
+    class: "Javascript",
+}
+console.log(typeof myObj);
+
+
+
+let name = 'ali';
+name = 'shahwaiz'; // variable reassigning
+console.log(name);
+
+
+const ID = 8597;
+ID = 8596;
+console.log(ID); // error hum const ko re assign nhi kar sakte
+
---
+++ 02.Operators/app.js
@@ -0,0 +1,45 @@
+
+/* Arithmetic Operations */
+
+// Addition
+let num1 = 2;
+let num2 = 2;
+// console.log(`Addition ${num1 + num2}`);
+
+// Subtraction
+let num3 = 2;
+let num4 = 2;
+// console.log(`Subtraction ${num3 - num4}`);
+
+// Multipliation
+let num5 = 2;
+let num6 = 2;
+// console.log(`Subtraction ${num5 * num6}`);
+
+// Division
+let num7 = 2;
+let num8 = 2;
+// console.log(`Division ${num7 / num8}`);
+
+// Modulus
+let num9 = 2;
+let num10 = 2;
+// console.log(`Modulus ${num7 % num8}`);
+
+
+/* Assignment Operator */
+
+// Addition 
+let number = 5;
+number += 4;
+// console.log(`Plus ${number}`);  
+
+// Subtraction 
+let number2 = 5;
+number2 -= 4;
+// console.log(`Minus ${number2}`);  
+
+
+/* Comparison Operator */
+
+console.log(8 > 7);
