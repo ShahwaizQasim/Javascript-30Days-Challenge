3b9188ca7175ce24013878a1468f9db7053de243
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-07-20 08:24:33 -0700

    task 4 in process

---
+++ 04.Loops/app.js
@@ -0,0 +1,51 @@
+
+/* Activity 1: For Loop */
+
+// task1
+for (let i = 1; i <= 10; i++) {
+    // console.log(i);
+}
+
+// task2
+for (let i = 1; i <= 10; i++) {
+    // console.log(i * 5);
+}
+
+
+/* Activity 2: While Loop */
+
+// task3 
+let sum = 0;
+let numbers = 1;
+while (numbers <= 10) {
+    sum += numbers; // 55
+    numbers++;
+}
+// console.log(sum); 
+
+// task4 
+let i = 10;
+while (i >= 1) {
+    // console.log(i);
+    i--;
+}
+
+
+/* Activity 3: do-while Loop */
+
+// task5
+let j = 1;
+do {
+    // console.log(j);
+    j++;
+} while (j <= 5);
+
+
+// task6
+let factorial = 1;
+let ii = 1;
+do {
+    factorial *= ii; // factorial
+    ii++;
+} while (ii <= 5);
+console.log(factorial); // 120
